216f286f988ad76b3df997ee8839c602917f4d6d
Libardo Lozano <lilozanoga@unal.edu.co>
2023-08-16 11:18:16 -0500

    test data

--- index.js
+++ index.js
@@ -6,21 +6,31 @@ const octokit = new Octokit({
 
 async function getStats() {
   try {
-    const user = await octokit.request('GET /user/repos', {
+    const repos = await octokit.request('GET /user/repos', {
       headers: {
         'X-GitHub-Api-Version': '2022-11-28'
       }
     })
 
-
-
-    console.log(user.data);
-
-    console.log("Tamaño: ",user.data.length);
-
-    console.log(user.data.map(repo => repo.name));
+    console.log(repos.data);
+    console.log("Tamaño: ",repos.data.length);
+    console.log(repos.data.map(repo => repo.name));
 
   } catch (error) {
+    console.error('Error:', error.message);
+  }
+
+  try{
+
+    const user = await octokit.request('GET /user', {
+      headers: {
+        'X-GitHub-Api-Version': '2022-11-28'
+      }
+    })
+
+    console.log(user.data);
+    
+  }catch(error){
     console.error('Error:', error.message);
   }
 }
