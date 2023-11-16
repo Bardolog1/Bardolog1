c115eee14fe95a2c601e153d9332585e7a249a54
Libardo Lozano Gambasica <64260884+Bardolog1@users.noreply.github.com>
2023-11-15 21:31:54 -0500

    Update index.js

--- index.js
+++ index.js
@@ -99,6 +99,9 @@ async function updateReadme(updatedStats) {
   const existingReadmeContent = await fs.readFile(readmePath, "utf-8");
 
   const updatedReadmeContent = `
+    <div align="center">
+      <img src="https://raw.githubusercontent.com/bardolog1/bardolog1/master/profile-3d-contrib/profile-night-rainbow.svg" alt="Contributions" width="100%"  />
+     </div>
     # Mi Proyecto de Readme Actualizado con GitHub Actions 
     
     Estadísticas actualizadas por GH Actions:
