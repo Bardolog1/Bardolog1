664ad7756da38fe4be5c0d3392519fa9ebe095e2
Libardo Lozano Gambasica <64260884+Bardolog1@users.noreply.github.com>
2023-11-16 11:53:40 -0500

    Update index.js

--- index.js
+++ index.js
@@ -103,9 +103,20 @@ async function updateReadme(updatedStats) {
 <div align="center">
   <img src="https://raw.githubusercontent.com/bardolog1/bardolog1/master/images/Banner developer (2).gif" alt="Contributions" width="100%"  />
 </div>
+<h2 align="center">Hi 👋! My name is Libardo Lozano  and I'm a Software Developer</h2>
+
+<div align="center">
+<p align="left">
+    Experienced software developer with a strong foundation in both programming and additional expertise in design and basic electronics. I bring over 3 years of hands-on experience in software development, coupled with excellent communication skills, a quick learning aptitude, leadership qualities, and a readiness to embrace change. 
+</p>
+<p align="left">
+    My passion for technology extends beyond software development. I possess a keen interest and basic knowledge in electronics, allowing me to bring a unique perspective to problem-solving. I thrive on curiosity and am committed to a continuous learning journey. Adaptable and open to change, I am well-equipped to navigate the dynamic landscape of the tech industry.
+</p>
+</div>
 <div align="center">
   <img src="https://raw.githubusercontent.com/bardolog1/bardolog1/master/profile-3d-contrib/profile-night-rainbow.svg" alt="Contributions" width="100%"  />
 </div>
+
     # Mi Proyecto de Readme Actualizado con GitHub Actions 
     
     Estadísticas actualizadas por GH Actions:
