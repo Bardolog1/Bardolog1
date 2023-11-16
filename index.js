a36a5940c1d475fc987b7727b2bdbc91b766d041
Libardo Lozano Gambasica <64260884+Bardolog1@users.noreply.github.com>
2023-11-16 13:36:41 -0500

    Update index.js

--- index.js
+++ index.js
@@ -106,14 +106,14 @@ async function updateReadme(updatedStats) {
 <h2 align="center">Hi 👋! My name is Libardo Lozano  and I'm a Software Developer</h2>
 
 <div align="center">
+<img align="right" src="https://media.giphy.com/media/6heBQSjt2IoA8/giphy.gif" width="400" height="200"/>
+  &nbsp;
+  &nbsp;
 <p align="left">
     Experienced software developer with a strong foundation in both programming and additional expertise in design and basic electronics. I bring over 3 years of hands-on experience in software development, coupled with excellent communication skills, a quick learning aptitude, leadership qualities, and a readiness to embrace change. 
 </p>
-<p align="left">
-    My passion for technology extends beyond software development. I possess a keen interest and basic knowledge in electronics, allowing me to bring a unique perspective to problem-solving. I thrive on curiosity and am committed to a continuous learning journey. Adaptable and open to change, I am well-equipped to navigate the dynamic landscape of the tech industry.
-    <iframe src="https://giphy.com/embed/6heBQSjt2IoA8" width="480" height="241" frameBorder="0" class="giphy-embed" allowFullScreen></iframe><p><a href="https://giphy.com/gifs/6heBQSjt2IoA8">via GIPHY</a></p>
-</p>
 </div>
+<br>
 <div align="center">
   <img src="https://raw.githubusercontent.com/bardolog1/bardolog1/master/profile-3d-contrib/profile-night-rainbow.svg" alt="Contributions" width="100%"  />
 </div>
