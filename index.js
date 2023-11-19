eeb68c05b1963caab257d51f04b4bf6c3a3b8751
libardo.lozanogambas <libardo.lozano.contractor@bbva.com>
2023-11-18 21:30:06 -0500

    Init GitHub Actions with  GitHub Actions

--- index.js
+++ index.js
@@ -99,29 +99,68 @@ async function updateReadme(updatedStats) {
   const existingReadmeContent = await fs.readFile(readmePath, "utf-8");
 
   const updatedReadmeContent = `
-  
 <div align="center">
-  <img src="https://raw.githubusercontent.com/bardolog1/bardolog1/master/images/Banner developer (2).gif" alt="Contributions" width="100%"  />
+<img src="https://raw.githubusercontent.com/Bardolog1/Bardolog1/master/images/Bannerdeveloper.gif" alt="Contributions" width="100%"  />
+<img width=100% src="https://capsule-render.vercel.app/api?type=waving&color=057EEF&height=120&section=header"/>
 </div>
+
 <h2 align="center">Hi 👋! My name is Libardo Lozano  and I'm a Software Developer</h2>
 
 <div align="center">
-<img align="right" src="https://media.giphy.com/media/6heBQSjt2IoA8/giphy.gif" width="400" height="200"/>
-  &nbsp;
-  &nbsp;
+<img src="https://raw.githubusercontent.com/Bardolog1/Bardolog1/master/images/dev1.png" min-width="400px" max-width="400px" width="400px" align="right" alt="Computador"/>
 <p align="left">
     Experienced software developer with a strong foundation in both programming and additional expertise in design and basic electronics. I bring over 3 years of hands-on experience in software development, coupled with excellent communication skills, a quick learning aptitude, leadership qualities, and a readiness to embrace change. 
 </p>
+<img width=100% src="https://capsule-render.vercel.app/api?type=waving&color=FF2967&height=120&section=footer"/>
 </div>
-<br>
-<div align="center">
-  <img src="https://raw.githubusercontent.com/bardolog1/bardolog1/master/profile-3d-contrib/profile-night-rainbow.svg" alt="Contributions" width="100%"  />
 </div>
 
-    # Mi Proyecto de Readme Actualizado con GitHub Actions
+
+<div align="center">
+<img src="https://raw.githubusercontent.com/Bardolog1/Bardolog1/master/profile-3d-contrib/profile-night-rainbow.svg" alt="Contributions" width="100%" aling="right"  />
+<!--<img width=45% align="center" src="https://github-readme-stats-bardolog1.vercel.app/api?username=bardolog1&show_icons=true&theme=dracula&show=repos&count_private=true&include_all_commits=true&hide=contribs,issues" aling="left"/>-->
+<img width=100% src="https://capsule-render.vercel.app/api?type=waving&color=057EEF&height=120&section=header"/>
+</div>
+
+<!--https://raw.githubusercontent.com/Bardolog1/Bardolog1/master-->
+
+# THIS README IS UNDER CONSTRUCTION
+
+## Principal Stack:
+<div aling="center">
+  <a href="#">
+    <img src="https://img.shields.io/badge/Java-007396?style=for-the-badge&logo=java&logoColor=white" target="_blank">
+  </a>
+  <a href="#">
+    <img src="https://img.shields.io/badge/Angular-DD0031?style=for-the-badge&logo=angular&logoColor=white" target="_blank">
+  </a>
+  <a href="#">
+    <img src="https://img.shields.io/badge/Spring-6DB33F?style=for-the-badge&logo=spring&logoColor=white" target="_blank">
+  </a>
+  <a href="#">
+    <img src="https://img.shields.io/badge/MySQL-005C84?style=for-the-badge&logo=mysql&logoColor=white" target="_blank">
+  </a>
+
+
+## Contacts:
+ <div align="left">
+<a href="mailto:liloga.dev@gmail.com">
+  <img src="https://img.shields.io/badge/-Gmail-%23333?style=for-the-badge&logo=gmail&logoColor=white" target="_blank">
+</a>
+<a href="https://www.linkedin.com/in/bardolog1/" target="_blank">
+  <img src="https://img.shields.io/badge/-LinkedIn-%230077B5?style=for-the-badge&logo=linkedin&logoColor=white" target="_blank">
+</a>
+<a href="https://t.me/bardolog1/" target="_blank">
+  <img src="https://img.shields.io/badge/Telegram-2CA5E0?style=for-the-badge&logo=telegram&logoColor=white" target="_blank">
+</a>
+<a href="https://www.instagram.com/bardolog1/" target="_blank">
+  <img src="https://img.shields.io/badge/WhatsApp-25D366?style=for-the-badge&logo=whatsapp&logoColor=white" alt="whatsapp logo">
+</a>
+
+ # Mi Proyecto de Readme Actualizado con GitHub Actions
 [![Run GH Actions & GH Stats](https://github.com/Bardolog1/Bardolog1/actions/workflows/runGHActions.yml/badge.svg)](https://github.com/Bardolog1/Bardolog1/actions/workflows/runGHActions.yml)    
     
-    Estadísticas actualizadas por GH Actions:
+    Estadísticas actualizadas por GH Actions (Falta perfeccionar):
     
     - Total de repositorios: ${updatedStats.totalPrivateRepos + updatedStats.totalPublicRepos}
     - Total de commits: ${updatedStats.totalCommits} 
