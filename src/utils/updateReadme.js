d815bb68b9b381285215daffee89441dded899ff
libardo.lozanogambas <libardo.lozano.contractor@bbva.com>
2024-05-09 15:57:31 -0500

    chore: Update updateReadme.js to log template output

--- src/utils/updateReadme.js
+++ src/utils/updateReadme.js
@@ -4,6 +4,7 @@ import ReadmeTemplate from "../templates/ReadmeTemplate.js";
 
 export async function updateReadme(updatedStats) {
   const readmePath = "../../README.md";
-
+  console.log("Updating README.md...");
+  console.log(ReadmeTemplate(updatedStats));
   await fs.writeFile(readmePath, ReadmeTemplate(updatedStats));
 }
