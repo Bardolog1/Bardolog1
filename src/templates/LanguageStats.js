ee967b26b9731189362909ab74ff44a187dcb196
libardo.lozanogambas <libardo.lozano.contractor@bbva.com>
2024-05-09 20:47:38 -0500

    feat: Remove commented out code in LanguageStats.js

--- src/templates/LanguageStats.js
+++ src/templates/LanguageStats.js
@@ -22,15 +22,3 @@ export default function LanguageStats(langsStats) {
     }).join('');
 }
 
-
-/* return langsStats?.slice(0, 5).map((lang, index) => {
-    return `
-<div align="center">
-    <img
-        src="https://raw.githubusercontent.com/github/explore/80688e429a7d4ef2fca1e82350fe8e3517d3494d/topics/${lang.name}/${lang.name}.png"
-        alt="Language ${lang.name}"
-        width="100%"
-    />
-</div>
-            `;
-  }).join(''); */
