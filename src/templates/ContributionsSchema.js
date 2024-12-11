97f45dfbb569f1f78bd534abb28f7a3657e69a48
Libardo Lozano Gambasica <64260884+Bardolog1@users.noreply.github.com>
2024-12-11 18:30:39 -0500

    Update ContributionsSchema.js

--- src/templates/ContributionsSchema.js
+++ src/templates/ContributionsSchema.js
@@ -7,10 +7,18 @@ export default function ContributionsSchema() {
         width="100%"
         aling="right"
     />
+
+     <picture>
+      <source media="(prefers-color-scheme: dark)" srcset="https://raw.githubusercontent.com/Bardolog1/Bardolog1/main/snake-contrib/github-snake-dark.svg" />
+      <source media="(prefers-color-scheme: light)" srcset="https://raw.githubusercontent.com/Bardolog1/Bardolog1/main/snake-contrib/github-snake.svg" />
+      <img alt="github-snake" src="github-snake.svg" />
+    </picture>
+    
     <img
         width="100%"
         src="https://capsule-render.vercel.app/api?type=waving&color=057EEF&height=120&section=header"
     />
+   
 </div>
     `;
 }
